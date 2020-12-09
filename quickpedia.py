c7c80122c95855d1605224a36a850e22af59ba0c
Ty <60036684+TyGillespie@users.noreply.github.com>
2020-12-08 17:32:04 -0700

    Added basic file writing support.

--- quickpedia.py
+++ quickpedia.py
@@ -9,21 +9,29 @@ import wikipedia
 
 import sys
 import argparse
+from typing import List
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Tool to look up things on Wikipedia.")
+    parser: argparse.ArgumentParser = argparse.ArgumentParser(
+        description="Tool to look up things on Wikipedia."
+    )
     parser.add_argument("search", help="What to search.")
-    parser.add_argument("-f","--file",action="store_true",help="Write the output to a file, not the terminal.")
-    args = parser.parse_args()
-    to_search = args.search
+    parser.add_argument(
+        "-f",
+        "--file",
+        action="store_true",
+        help="Write the output to a file, not the terminal.",
+    )
+    args: List[str] = parser.parse_args()
+    to_search: str = args.search
     if to_search == "":
         print("A queery wasn't provided.")
         sys.exit()
     else:
-        res = wikipedia.summary(to_search)
+        res: str = wikipedia.summary(to_search)
         if args.file:
-            f = open(to_search+".txt","w")
+            f = open(to_search + ".txt", "w")
             f.write(res)
             f.close()
         else:
